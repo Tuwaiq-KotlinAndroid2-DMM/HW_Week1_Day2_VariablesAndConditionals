4af33c5c706e2ec7791601958800e6977e5336b5
Nora Almusbeh <noraalmusbeh@MacBook-Pro-alkhas-b-Nora.local>
2021-10-11 19:20:46 +0300

    I added my second  Homework SumNumbers

---
+++ Day2MySecondHW/src/main/kotlin/SumNumbers.kt
@@ -0,0 +1,14 @@
+fun main(){
+
+    println("Please enter the first number : ")
+    var first =Integer.valueOf(readLine())
+
+
+    println("Please enter the second number : ")
+    var second =Integer.valueOf(readLine())
+
+    var result = first + second
+
+    println("The result is : " + result)
+
+}
