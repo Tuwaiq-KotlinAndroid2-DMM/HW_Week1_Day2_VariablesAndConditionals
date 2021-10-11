462822e89dc448d3e76fbf5c56f0794fc78d2946
unknown <raialz9090@gmail.com>
2021-10-11 22:21:38 +0300

    first HW

---
+++ HW1/src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+fun main() {
+
+    val n1 = 25
+    val n2 = 78
+    val n3 = 87
+
+    if(n1 >= n2 && n1>= n3 )
+        println("$n1 is Greatest number.")
+    else if (n2 >= n1 && n2 >= n3 )
+        println("$n2 is Greatest number.")
+    else
+        println("$n3 is Greatest number.")
+}
---
+++ Hw2/src/main/kotlin/Main.kt
@@ -0,0 +1,8 @@
+fun main() {
+   val num1 = 74
+   val num2 = 36
+
+   val  sum = num1 + num2
+
+   println("The sum of 2 numbers is $sum")
+}
