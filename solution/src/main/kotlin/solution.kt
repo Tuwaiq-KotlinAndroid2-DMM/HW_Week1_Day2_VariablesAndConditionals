d50945070ea68161b3fe58e71141d2da2704931f
mashael alotaibi <92297149+mashael13@users.noreply.github.com>
2021-10-11 20:47:18 +0300

    Update solution.kt

--- solution/src/main/kotlin/solution.kt
+++ solution/src/main/kotlin/solution.kt
@@ -1,9 +1,10 @@
 fun main(){
 
-    println("enter three numbers")
-
+    println("enter first number")
     var n1 = Integer.valueOf(readLine())
+    println("enter seconed number")
     var n2= Integer.valueOf(readLine())
+    println("enter third number")
     var n3 = Integer.valueOf(readLine())
    if ( n1 > n2 && n1 >n3) {
         println("The greatest: " + n1)
