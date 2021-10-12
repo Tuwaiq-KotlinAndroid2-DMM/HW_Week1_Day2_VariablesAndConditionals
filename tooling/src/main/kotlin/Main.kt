f9d02f5ae8316bfd23408e6e660cdfcc7f9cbbb0
turkicd <you@example.com>
2021-10-12 08:51:23 +0300

    homework updated

---
+++ tooling/src/main/kotlin/Main.kt
@@ -0,0 +1,24 @@
+fun main(args: Array<String>) {
+
+    //val a: Int = 74
+    //val b: Int = 36
+    //val sum = a + b
+    // println("the sum is $sum")
+    // hw2
+
+    val a = 25
+    val b = 78
+    val c = 87
+
+    if (a>b && a>c) {
+        println("the greatest: " +a)
+    }
+    else if (b>a && b>c) {
+        println("the greatest: " +b)
+
+    }
+    else {
+        println("the greatest;" + c)
+    }
+
+}
