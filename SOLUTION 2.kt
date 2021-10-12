e40ece4e77d30391d1f39d403e400435605a95a2
unknown <Najwa.Ali.dmm.k2@tuwaiq.edu.sa>
2021-10-12 08:06:49 +0300

    my homework

---
+++ SOLUTION 2.kt
@@ -0,0 +1,19 @@
+fun main() {
+    // The user enter the numbers
+    println("please enter the first number to sum:")
+    var x = Integer.valueOf(readLine())
+    println("please enter the second number to sum:")
+    var y = Integer.valueOf(readLine())
+    var z=x+y
+    println ("The sum of these two numbers is =" + z)
+   // chosen variables
+/*
+    var x =74
+     var y =36
+     var z = x + y
+     println("$x + $y =" +z)
+ */
+
+
+}
+
---
+++ solution1.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>) {
+var  x =25
+    var  y =78
+    var  z =87
+    if(x>y && x>z ) {
+        println("The greatest:" + x)
+    }
+    else if (y>x && y>z){
+        println ("The greatest:" + y)
+
+    }
+else {
+    println ("The greatest:"+ z)
+
+    }
+}
