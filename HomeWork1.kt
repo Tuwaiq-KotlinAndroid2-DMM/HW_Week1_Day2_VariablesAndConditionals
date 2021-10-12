b09627e944cc7c8188e09406ec3e551af392c796
ghzlan <ghzlan-1996-a@hotmail.com>
2021-10-12 07:44:29 +0300

    My first homework day2

---
+++ HomeWork1.kt
@@ -0,0 +1,23 @@
+fun main(){
+     println("please entar 3 numper")
+var number1 =  Integer.valueOf(readLine())
+     var number2 = Integer.valueOf(readLine())
+var number3 = Integer.valueOf(readLine())
+
+     if (number1 > number2 && number1 > number3){
+
+                  println("the gratest: $number1")
+
+     }
+             else if (number2 > number1 && number2 > number3){
+
+
+                     println("the gratest: $number2")
+                 }
+
+     else if (number3 > number1 && number3 > number2){
+
+          println("The gratest: $number3")
+                 }
+             }
+
---
+++ HomeWork2.kt
@@ -0,0 +1,8 @@
+ fun main() {
+           println("Enter two numbrs to add:")
+          var num1 = Integer.valueOf(readLine())
+           var num2 = Integer.valueOf(readLine())
+
+     var sum = num1 + num2
+     println(sum)
+ }
